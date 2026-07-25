2ba1b69e93b9dbf0010cb9522ce5630d87dd476c
DiegoAxe <diegoarielmar@hotmail.com>
2026-07-25 02:16:33 -0600

    Improve ProductGrid with Next/Image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  /* Configuracion para que acepte las imagenes procedientes de Amazon */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
+  },
+
   reactCompiler: true,
 };
 
--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -2,6 +2,9 @@
 
 "use client";
 
+/* Para que funcione next/Image */
+import Image from "next/image";
+
 import { listProducts } from "../data/listProducts";
 import { addToCart } from "../redux/cartSlice";
 import { useAppDispatch } from "../redux/hooks";
@@ -15,14 +18,21 @@ export default function ProductGrid() {
     <div className="product-grid">
       {listProducts.map((product) => (
         <div key={product.id} className="product-card">
-          <img src={product.image} alt={product.title} />
+
+          <div className="product-image">
+            {/* Aqui se esta utilizando el next/Image */}
+            <Image src={product.image} alt={product.title} width={200} height={200}  />
+          </div>
+
           <div className="product-info">
-            <h3 className="product-title">{product.title}</h3>
+            <h1 className="product-title">{product.title}</h1>
+            <h3 className="product-category">{product.category}</h3>
             <p className="product-price">${product.price}</p>
             <button className="add-button" onClick={() => dispatch(addToCart(product))}>
               Agregar al carrito
             </button>
           </div>
+
         </div>
       ))}
     </div>
