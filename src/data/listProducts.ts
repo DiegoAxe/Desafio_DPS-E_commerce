e91883ce7ceb6cd53b682a75c1ba5758e42843d0
DiegoAxe <diegoarielmar@hotmail.com>
2026-07-24 23:49:35 -0600

    Add products list

---
+++ src/data/listProducts.ts
@@ -0,0 +1,144 @@
+import { Product } from "../types/Product";
+
+export const listProducts: Product[] = [
+  {
+    id: 1,
+    title: 'Ropa1',
+    price: 100,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 1
+  },
+  {
+    id: 2,
+    title: 'Ropa2',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 3,
+    title: 'Ropa3',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 4,
+    title: 'Ropa4',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 5,
+    title: 'Ropa5',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 6,
+    title: 'Ropa6',
+    price: 100,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 1
+  },
+  {
+    id: 7,
+    title: 'Ropa7',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 8,
+    title: 'Ropa8',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 9,
+    title: 'Ropa9',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 10,
+    title: 'Ropa10',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 11,
+    title: 'Ropa11',
+    price: 100,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 1
+  },
+  {
+    id: 12,
+    title: 'Ropa12',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 13,
+    title: 'Ropa13',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 14,
+    title: 'Ropa14',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 15,
+    title: 'Ropa15',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 16,
+    title: 'Ropa16',
+    price: 100,
+    image: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity: 1
+  },
+  {
+    id: 17,
+    title: 'Ropa17',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 18,
+    title: 'Ropa18',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 19,
+    title: 'Ropa19',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  },
+  {
+    id: 20,
+    title: 'Ropa20',
+    price: 190,
+    image: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity: 1
+  }
+];
